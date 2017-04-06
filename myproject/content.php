288eeee0865ad2a17c32ab2f73573fa4004018af
OliaVM <oliamo2016@gmail.com>
2017-04-07 03:38:15 +0700

    file

--- myproject/content.php
+++ myproject/content.php
@@ -1,15 +1,13 @@
 					    <?php require '/var/www/html/src/core/form/add_news_form.php'; ?>
 					</div>
 					<div>
-			 <?php	
-			  	if (isset($x)) { // выполняем исключение, если оно есть
-						echo "<h2>" . $x . "</h2>";
-				}
-				if (isset($x2)) { // выполняем исключение, если оно есть
-						echo "<h2>" . $x2 . "</h2>";
-				}
-
-			?>
+			 
+			 <?php	if (isset($x)): ?> 
+				<?php echo "<h2>" . $x . "</h2>";?>
+			<?php	endif; ?> 
+			<?php	if (isset($x2)): ?>  
+				<?php echo "<h2>" . $x2 . "</h2>";?>
+			<?php	endif; ?> 
 			<?php $sql2 = "SELECT * FROM articles"; ?>
 			<?php $news_list = $basa->query($sql2); ?>
 			<?php require '/var/www/html/myproject/common/news_list.php'; ?>
