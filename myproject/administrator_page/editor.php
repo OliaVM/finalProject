33cb87ebba7eb30ecc18f7c46fe74ae1fe783cf8
OliaVM <oliamo2016@gmail.com>
2017-04-27 01:11:16 +0700

    Update editor.php

--- myproject/administrator_page/editor.php
+++ myproject/administrator_page/editor.php
@@ -4,24 +4,24 @@
 if (isset($_GET['red_id'])) { 
     	$id = $_GET['red_id']; 
        	$sql3 = 'SELECT * FROM articles WHERE id='.$id.'';
-		$row5 = $basa->query($sql3); 
-		//$sth = $basa->prepare($sql); 
-		//$sth->execute();
-		//$row5 = $sth->fetch(PDO::FETCH_ASSOC); //Преобразуем ответ из БД в строку массива
+	$row5 = $basa->query($sql3); 
+	//$sth = $basa->prepare($sql); 
+	//$sth->execute();
+	//$row5 = $sth->fetch(PDO::FETCH_ASSOC); //Преобразуем ответ из БД в строку массива
 }
 //Редактирование записи
 if (isset($_GET['red_id'])) { //обновляем информацию в отредавтированной записи (в БД)
         if (isset($_POST['go'])) { 
         	$sql = 'UPDATE articles  SET  article_name='.$_POST['title'].' WHERE id='.$_GET['red_id'].'';
         	$result = $basa->query($sql);
-			//$sql5 = 'UPDATE articles  SET  article_name='.$_POST['title'].', article_text='.$_POST['message'].' , rubrika='.$_POST['theme'].'  WHERE id='.$_GET['red_id'].'';
-			$message = $_POST['message'];
-			$sql2 = 'UPDATE articles  SET  article_text='.$message.' WHERE id='.$_GET['red_id'].'';
-			$result2 = $basa->query($sql2);
+		//$sql5 = 'UPDATE articles  SET  article_name='.$_POST['title'].', article_text='.$_POST['message'].' , rubrika='.$_POST['theme'].'  WHERE id='.$_GET['red_id'].'';
+		$message = $_POST['message'];
+		$sql2 = 'UPDATE articles  SET  article_text='.$message.' WHERE id='.$_GET['red_id'].'';
+		$result2 = $basa->query($sql2);
 			
-			$sql3 = 'UPDATE articles  SET  rubrika='.$_POST['theme'].' WHERE id='.$_GET['red_id'].'';
-			$result3 = $basa->query($sql3);
-		}
+		$sql3 = 'UPDATE articles  SET  rubrika='.$_POST['theme'].' WHERE id='.$_GET['red_id'].'';
+		$result3 = $basa->query($sql3);
+	}
     }
 ?>
 
